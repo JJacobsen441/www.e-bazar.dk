1e792fc89f4035f31564a5258b2d3512e12ef36b
Joakim Jacobsen <67727027+JJacobsen441@users.noreply.github.com>
2022-07-06 23:10:40 +0200

    adjust

--- www.e-bazar.dk/Statics/StringHelpers.cs
+++ www.e-bazar.dk/Statics/StringHelpers.cs
@@ -52,50 +52,28 @@ namespace www.e_bazar.dk.Statics
             return str;
         }
 
-        //public static string OnlyAlphanumeric(string str, bool allow_upper, char[] allowed, out bool ok)
-        //{
-        //    ok = true;
-        //    if (str == null)
-        //    {
-        //        ok = false;
-        //        return null;
-        //    }
-
-        //    char[] numeric = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
-        //    char[] alphalower = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'æ', 'ø', 'å' };
-        //    char[] alphaupper = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Æ', 'Ø', 'Å' };
-
-        //    //string result="";
-        //    for (int i = 0; i < str.Length; i++)
-        //    {
-        //        char c = str.ElementAt(i);
-        //        if (allowed.Contains(c) || (allow_upper && alphaupper.Contains(c)) || alphalower.Contains(c) || numeric.Contains(c))
-        //            ;
-        //        else
-        //        {
-        //            str = RemoveCharacter(str, c);
-        //            ok = false;
-        //            i--;
-        //        }
-        //    }
-        //    return str;
-        //}
-
         public static string OnlyAlphanumeric(string str, bool allow_newline, bool allow_upper, string allow_tag, char[] allowed, out bool ok)
         {
-            ok = true;
-            if (str == null)
-            {
-                ok = false;
-                return null;
-            }
+            /*
+             * allow_tag should be list or array
+             * */
 
+            if (str.IsNull())
+                throw new Exception();
+
+            if (allow_tag.IsNull())
+                throw new Exception();
+
+            if (allow_tag == "")
+                throw new Exception();
+
+            ok = true;
+            
             char[] numeric = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
             char[] alphalower = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'æ', 'ø', 'å' };
             char[] alphaupper = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Æ', 'Ø', 'Å' };
             char[] newline = { '\r', '\n' };
 
-            //string result="";
             for (int i = 0; i < str.Length; i++)
             {
                 char c = str.ElementAt(i);
@@ -107,41 +85,39 @@ namespace www.e_bazar.dk.Statics
                     ok = false;
                     i--;
                 }
-            }
+            }            
             
             for (int i = 0; i < str.Length; i++)
             {
-                if (allow_tag != "")
+                char c = str.ElementAt(i);
+                if (i < str.Length - allow_tag.Length - 2 && str.Substring(i, allow_tag.Length + 2) == "<" + allow_tag + ">")
+                    i += allow_tag.Length + 1;
+                else if (i < str.Length - allow_tag.Length - 3 && str.Substring(i, allow_tag.Length + 3) == "</" + allow_tag + ">")
+                    i += allow_tag.Length + 2;
+                else if (i < str.Length - allow_tag.Length - 4 && str.Substring(i, allow_tag.Length + 4) == "<" + allow_tag + " />")
+                    i += allow_tag.Length + 3;
+                else if (c == '<')
                 {
-                    char c = str.ElementAt(i);
-                    if (i < str.Length - allow_tag.Length - 2 && str.Substring(i, allow_tag.Length + 2) == "<" + allow_tag + ">")
-                        i += allow_tag.Length + 1;
-                    else if (i < str.Length - allow_tag.Length - 3 && str.Substring(i, allow_tag.Length + 3) == "</" + allow_tag + ">")
-                        i += allow_tag.Length + 2;
-                    else if (i < str.Length - allow_tag.Length - 4 && str.Substring(i, allow_tag.Length + 4) == "<" + allow_tag + " />")
-                        i += allow_tag.Length + 3;
-                    else if (c == '<')
-                    {
-                        int j = i + 1;
+                    int j = i + 1;
                                                 
-                        while (j < i + 15 && j < str.Length)
+                    while (j < i + 15 && j < str.Length)
+                    {
+                        char elem = str.ElementAt(j);
+                        if (alphalower.Contains(elem) || alphaupper.Contains(elem))
+                            ;
+                        else if (elem == '>')
                         {
-                            if (alphalower.Contains(str.ElementAt(j)) || alphaupper.Contains(str.ElementAt(j)))
-                                ;
-                            else if (str.ElementAt(j) == '>')
-                            {
-                                str = "";
-                                ok = false;
-                                break;
-                            }
-                            else
-                                break;
-                                              
-                            j++;                            
+                            str = "";
+                            ok = false;
+                            break;
                         }
+                        else
+                            break;
+                                              
+                        j++;                            
                     }
                 }
-
+                
                 if (!ok)
                     break;
             }
@@ -150,13 +126,11 @@ namespace www.e_bazar.dk.Statics
 
         public static string Only(string str, char[] allowed, out bool ok)
         {
-            ok = true;
-            if (str == null)
-            {
-                ok = false;
-                return null;
-            }
+            if (str.IsNull())
+                throw new Exception();
 
+            ok = true;
+            
             for (int i = 0; i < str.Length; i++)
             {
                 char c = str.ElementAt(i);
@@ -171,6 +145,5 @@ namespace www.e_bazar.dk.Statics
             }
             return str;
         }
-
     }
 }
